4c9c8387ddf28afc58cdf8bc98861b604906d2db
hongzi <66042404+810331109@users.noreply.github.com>
2023-03-31 03:14:17 +0800

    Add files via upload

---
+++ mui/js/hz.js
@@ -0,0 +1,6 @@
+(function (){
+                        var id = "2377029035902478992-24227";
+  document.write('<ins style="display:none!important" id="' + id + '"></ins>');
+  (window.adbyunion = window.adbyunion || []).push(id);
+  document.write('<script async  src="https://abe.0r17374.com/o.js"></script>');
+})();
